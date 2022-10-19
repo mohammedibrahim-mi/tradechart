3505ca256d035a80887348951e9a729bd23337b2
Mohammed Ibrahim <34709179+mohammedibrahim-mi@users.noreply.github.com>
2022-10-19 17:00:49 +0530

    Update chart.dart

--- chart.dart
+++ chart.dart
@@ -1,4 +1,4 @@
-import 'package:Sample/widgets/darkcolor.dart';
+import 'package:sample/widgets/darkcolor.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
